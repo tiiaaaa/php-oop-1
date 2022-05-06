2105cb47ec4106896cf8f9fb889340bcb4b72c80
MattiaPortanti <mattia.portantipm@gmail.com>
2022-05-06 16:11:31 +0200

    inizializzato una classe Movie con variabili e costruttore

--- index.php
+++ index.php
@@ -1,4 +1,23 @@
+<?php
+// Dichiarata la classe con le sue variabili
+class Movie{
+    public $title;
+    public $poster;
+    public $description;
+    public $genre;
+    public $timeLenght;
 
+    // Creato un costruttore
+    function __construct($_title, $_poster, $_description, $_genre, $_timeLenght){
+            $this -> title = $_title;
+            $this -> poster = $_poster;
+            $this -> description = $_description;
+            $this -> genre = $_genre;
+            $this -> timeLenght = $_timeLenght
+    }
+}
+
+?>
 
 <!DOCTYPE html>
 <html lang="en">
